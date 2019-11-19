b6d88b825d067db3e014f3bed40cfca9c0d39df2
939346349 <13050400097@163.com>
2019-11-19 18:51:35 +0800

    init project

---
+++ adminapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30,unique=True)
+    password = models.CharField(max_length=150)
+    age = models.IntegerField(null=False)
